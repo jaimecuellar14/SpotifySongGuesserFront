da4212a03322560f7b3fbec7620c7cf516f3ee09
jaimecuellar14 <jamescuellar14@gmail.com>
2021-06-10 11:02:21 +0200

    Creation of login form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private formBuilder: FormBuilder){}
   title = 'SpotifySongGuesserFront';
+
+  loginForm = new FormGroup({
+    email: new FormControl(),
+    password: new FormControl()
+  });
+  // tslint:disable-next-line:typedef
+  onSubmit(){
+    console.log(this.loginForm.get('email').value);
+    console.log(this.loginForm.get('password').value);
+  }
+  // tslint:disable-next-line:typedef
+  ngOnInit(){
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email
+      ]),
+      password: new FormControl('', [
+        Validators.required
+      ])
+    });
+  }
 }
---
+++ src/app/models/user.ts
@@ -0,0 +1,11 @@
+export class User {
+
+    constructor(public id: number, public name: string, public lastName: string, public email: string,
+    )
+    {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
