869ca23c18b4e8a5085dde1aff0f19eaa7a24f3a
jaimecuellar14 <jamescuellar14@gmail.com>
2021-06-14 12:40:22 +0200

    SSG-7 #comment Creation of new branch to register page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule} from '@angular/common/http';
+import { RegisterComponentComponent } from './components/register-component/register-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    RegisterComponentComponent,
   ],
   imports: [
     BrowserModule,
