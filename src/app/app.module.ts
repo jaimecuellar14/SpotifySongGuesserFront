6ce367d8a738b8ceb0a21dfa9e57b94f5ce49137
jaimecuellar14 <jamescuellar14@gmail.com>
2021-06-14 13:19:49 +0200

    SSG-10 #Fixed routing issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule} from '@angular/common/http';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
